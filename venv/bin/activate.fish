d0aca2a63ed1c5c8473947d4eb8e183927e508cd
HASKADOG <cplasplas16@gmail.com>
2020-11-23 16:59:25 +0300

    1.2

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/haska/Work/teledeck/venv"
+set -gx VIRTUAL_ENV "/home/haska/work/teledeck/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
